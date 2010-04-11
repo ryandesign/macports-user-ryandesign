e8dcf30b6830292edc19d1873e82f7ef14985d17
Ryan Schmidt <ryandesign@macports.org>
2010-04-11 01:05:57 +0000

    mystonline-cider:
     * update to use mystonline-bootstrap
     * fix parent icon bouncing in the dock too long at startup
     * add comments
     * update notes
    
    git-svn-id: https://svn.macports.org/repository/macports/users/ryandesign@66394 d073be05-634f-4543-b044-5fe20cf6d1d6

--- ports/games/mystonline-cider/files/MOUL.sh
+++ ports/games/mystonline-cider/files/MOUL.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# This script runs the game launcher (if it's already been installed)
-# or the installer (if it hasn't).
+# This script runs the game launcher, installing it if it's not already there.
 
 export WINEPREFIX="@WINEPREFIX@"
 
@@ -18,4 +17,4 @@ if [ ! -r "$LAUNCHER" ]; then
     cp -p "$BOOTSTRAP_LAUNCHER" "$LAUNCHER"
 fi
 
-"$(dirname "$0")/../Resources/Myst Online.app/Contents/MacOS/cider"
+"$(dirname "$0")/../Resources/Myst Online.app/Contents/MacOS/cider" &
